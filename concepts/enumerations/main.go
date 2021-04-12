ef6e9d1345256f71843aa7287ee2f0975328b0c5
Blitzen Prancer <76688640+BlitzenPrancer@users.noreply.github.com>
2021-04-12 21:19:25 +0530

    Create main.go

---
+++ concepts/enumerations/main.go
@@ -0,0 +1,23 @@
+// enumerations can be defined by types and constants in go
+package main
+
+import "fmt"
+
+// DayOfWeek represents a day
+type DayOfWeek int
+
+// constants of type DayOfWeek
+const (
+	// sunday DayOfWeek = 1 << iota - this assigns constants exponential value
+	sunday DayOfWeek = 1 + iota
+	monday
+	tuesday
+	wednesday
+	thursday
+	friday
+	saturday
+)
+
+func main() {
+	fmt.Println(tuesday)
+}
